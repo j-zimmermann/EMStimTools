3cc150962b76da156b6d01810ce7a0bc5bd2dbc7
Julius Zimmermann <julius.zimmermann@uni-rostock.de>
2020-03-26 16:42:15 +0100

    added matplotlib

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(
     author_email='julius.zimmermann@uni-rostock.de',
     packages=['emstimtools', 'emstimtools.utils', 'emstimtools.fenics', 'emstimtools.dataanalysis'],
     description='EMStimTools is a package that provides a SALOME-GMSH-FEniCS workflow to solve problems related to electromagnetic stimulation of for instance biological tissue or cell cultures.',
-    install_requires=['pyyaml==5.3'],
+    install_requires=['pyyaml==5.3', 'matplotlib>=3.1.3],
     long_description=open('README.rst').read()
 )
